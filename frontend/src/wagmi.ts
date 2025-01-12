08917e216fb93defd5f7f4fc3d26a05e93049dfc
Greg May <mrgregmay@gmail.com>
2025-01-11 17:28:04 -0800

    chore: set polygon RPC

--- frontend/src/wagmi.ts
+++ frontend/src/wagmi.ts
@@ -15,7 +15,7 @@ export const config = createConfig({
   transports: {
     [arbitrum.id]: http('https://arb1.arbitrum.io/rpc'),
     [base.id]: http('https://base.drpc.org'),
-    [polygon.id]: http(''),
+    [polygon.id]: http('https://polygon-rpc.com'),
     [sepolia.id]: http('https://sepolia.drpc.org'),
   },
 })
